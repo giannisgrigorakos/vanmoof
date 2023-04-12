9bf26af6edb7bfeb286cf270e1493e64052f5fcf
giannisgrigorakos <grigorakos.giannis@gmail.com>
2023-04-12 15:14:13 +0300

    Explain why the bug happened in a comment

--- internal/mexicanWave/mexicanWave.go
+++ internal/mexicanWave/mexicanWave.go
@@ -8,7 +8,9 @@ import (
 // Wave takes a word as input and returns a Mexican Wave of this word as a slice of strings.
 func Wave(input string) []string {
 	lowerInput := strings.ToLower(input)
-	result := []string{}
+	result := []string{} // When instantiating something with the var keyword Golang will give it the default value. The default value for
+	// slices is nil. So in the test we were comparing nil with an empty slice of strings that's why we got the wrong result. So we need to
+	// define as above in order to avoid the drawback of nil slice declaration.
 
 	for i, char := range lowerInput {
 		examChar := string(char)
